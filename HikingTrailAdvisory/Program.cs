8625e33b0d59229e79af64db834cd413ea2bebba
Theodor Suciu <theodorsuciu@yahoo.com>
2024-02-02 16:31:13 -0800

    Adding Selenium

--- HikingTrailAdvisory/Program.cs
+++ HikingTrailAdvisory/Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using HtmlAgilityPack;
+﻿using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager;
 
 namespace HikingTrailAdvisory
 {
@@ -13,10 +14,34 @@ namespace HikingTrailAdvisory
             var client = new HttpClient();
             var html = await client.GetStringAsync(url);
 
-            ScrapeHikeData(html);
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            IWebDriver driver = new ChromeDriver();
+
+            List<string> hikeLinks = ScrapeHikeLinks(driver);
+
+            List<string> hikeNames = ScrapeHikeNames(html);
         }
 
-        static void ScrapeHikeData(string html)
+        private static List<string> ScrapeHikeLinks(IWebDriver driver)
+        {
+            // Navigate to the page containing the hikes
+            driver.Navigate().GoToUrl("https://www.wta.org/go-outside/hikes");
+
+            // Find all <a> elements
+            var links = driver.FindElements(By.TagName("a"));
+
+            // Filter to get only the links that lead to hike details
+            List<string> hikeLinks = links.Where(link => link.GetAttribute("href").Contains("/go-hiking/hikes/")).Select(link => link.GetAttribute("href")).Distinct().ToList();
+
+            return hikeLinks;
+
+            /*foreach (var link in hikeLinks)
+            {
+                Console.WriteLine(link);
+            }*/
+        }
+
+        static List<string> ScrapeHikeNames(string html)
         {
             var htmlDocument = new HtmlDocument();
             htmlDocument.LoadHtml(html);
@@ -25,7 +50,7 @@ namespace HikingTrailAdvisory
 
             if (hikeNodes != null)
             {
-                foreach ( var hikeNode in hikeNodes )
+                foreach (var hikeNode in hikeNodes)
                 {
                     var hikeName = hikeNode.GetAttributeValue("data-hikename", string.Empty);
                     Console.WriteLine(hikeName);
