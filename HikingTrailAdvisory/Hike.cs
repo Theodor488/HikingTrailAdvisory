28e7896900596a878866552bd55d366e64603d89
Theodor Suciu <theodorsuciu@yahoo.com>
2024-02-05 07:42:06 -0800

    Hike Class

---
+++ HikingTrailAdvisory/Hike.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HikingTrailAdvisory
+{
+    public class Hike
+    {
+        // Properties
+        public string Name { get; set; }
+        public float Length { get; set; }
+        public int ElevationGain { get; set; }
+        public int HighestPoint { get; set; }
+        public int[] Coords { get; set; }
+        public string Difficulty { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+
+        // Constructor
+        public Hike(string name, float length, int elevationGain, int highestPoint, int[] coords, string difficulty, string description, string link) 
+        {
+            Name = name;
+            Length = length;
+            ElevationGain = elevationGain;
+            HighestPoint = highestPoint;
+            Coords = coords;
+            Difficulty = difficulty;
+            Description = description;
+            Link = link;
+        }
+    }
+}
